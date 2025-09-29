1b8450382453cd339b7e4d216588abafc0ee6b65
iutgeiitoulonE105 <touloniut@gmail.com>
2025-09-29 15:46:12 +0200

    ca compile :)

---
+++ Robot_Collange_Janvier.X/ChipConfig.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   ChipConfig.h
+ * Author: E306-PC3
+ *
+ * Created on September 29, 2025, 3:22 PM
+ */
+
+
+#ifndef CHIPCONFIG_H
+#define	CHIPCONFIG_H
+
+#define FCY 60000000
+void InitOscillator();
+
+#endif	/* CHIPCONFIG_H */
+
